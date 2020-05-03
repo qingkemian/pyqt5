157014b1d2c33ed8a42e1fe71db87f29a4b0280d
qingkemian <2060097866@qq.com>
2020-05-03 17:02:46 +0800

    pyqt5demo

--- test/MyMain.py
+++ test/MyMain.py
@@ -4,6 +4,8 @@ import sys
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+import pandas as pd
+import numpy as np
 
 class Example(QMainWindow):
     def __init__(self):
@@ -32,6 +34,34 @@ class Example(QMainWindow):
         toolbar.addAction(exitAct)
 
         # 设置主窗口的中心窗口的控件
+
+        ### 顶部打开按钮
+        btn1 = QPushButton("打开")
+        btn1.clicked.connect(self.openfile)
+        btn1.clicked.connect(self.creat_table_show)
+
+        hbox_open = hbox = QHBoxLayout()
+        hbox_open.addStretch(1)
+        hbox_open.addWidget(btn1)
+
+        ### 表格
+        self.tableWidget = QTableWidget()
+
+        radio_one_layout = QHBoxLayout()
+        self.button1 = QRadioButton('单选按钮1')
+        self.button1.setChecked(True)
+
+        self.button1.toggled.connect(self.buttonState)
+        radio_one_layout.addWidget(self.button1)
+
+        self.button2 = QRadioButton('单选按钮2')
+        self.button2.toggled.connect(self.buttonState)
+        radio_one_layout.addWidget(self.button2)
+
+        btn3 = QPushButton("按钮3")
+        textEdit = QTextEdit()
+
+        ### 底部
         okButton = QPushButton("确定")
         cancelButton = QPushButton("取消")
 
@@ -41,15 +71,11 @@ class Example(QMainWindow):
         hbox.addWidget(cancelButton)
 
         vbox = QVBoxLayout()
-        btn1 = QPushButton("按钮1")
-        btn2 = QPushButton("按钮2")
-        btn3 = QPushButton("按钮3")
-        textEdit = QTextEdit()
-
         vbox.addStretch(0)
+        vbox.addLayout(hbox_open)
+        vbox.addWidget(self.tableWidget)
         vbox.addWidget(textEdit)
-        vbox.addWidget(btn1)
-        vbox.addWidget(btn2)
+        vbox.addLayout(radio_one_layout)
         vbox.addWidget(btn3)
         vbox.addStretch(1)
         vbox.addLayout(hbox)
@@ -61,13 +87,18 @@ class Example(QMainWindow):
         self.setCentralWidget(self.mdi)
 
         # 设置停靠控件1
-        self.items = QDockWidget('Dockable', self)
+        self.items = QDockWidget('Project（别碰 软件会崩）', self)
         self.listWidget = QListWidget()
-        self.listWidget.addItem('item1')
-        self.listWidget.addItem('item2')
-        self.listWidget.addItem('item3')
+        self.model = QFileSystemModel()
+        self.model.setRootPath('')
+        self.tree = QTreeView()
+        self.tree.setModel(self.model)
+        self.tree.setAnimated(False)
+        self.tree.setIndentation(20)
+        self.tree.setSortingEnabled(True)
+        self.tree.setWindowTitle("Dir View")
 
-        self.items.setWidget(self.listWidget)
+        self.items.setWidget(self.tree)
 
         # self.items.setFloating(True)
 
@@ -108,6 +139,46 @@ class Example(QMainWindow):
             self.mdi.tileSubWindows()
         else:
             pass
+
+    # 获取文件路径
+    def openfile(self):
+        openfile_name = QFileDialog.getOpenFileName(self,'选择文件','','Excel files(*.xlsx , *.xls)')
+        global path_openfile_name
+        path_openfile_name = openfile_name[0]
+
+    # 读取表格、显示表格
+    def creat_table_show(self):
+        if len(path_openfile_name) > 0:
+            input_table = pd.read_excel(path_openfile_name)
+            input_table_rows = input_table.shape[0]
+            input_table_colunms = input_table.shape[1]
+            input_table_header = input_table.columns.values.tolist()
+
+            self.tableWidget.setColumnCount(input_table_colunms)
+            self.tableWidget.setRowCount(input_table_rows)
+            self.tableWidget.setHorizontalHeaderLabels(input_table_header)
+
+            for i in range(input_table_rows):
+                input_table_rows_values = input_table.iloc[[i]]
+                input_table_rows_values_array = np.array(input_table_rows_values)
+                input_table_rows_values_list = input_table_rows_values_array.tolist()[0]
+                for j in range(input_table_colunms):
+                    input_table_items_list = input_table_rows_values_list[j]
+                    input_table_items = str(input_table_items_list)
+                    newItem = QTableWidgetItem(input_table_items)
+                    newItem.setTextAlignment(Qt.AlignHCenter|Qt.AlignVCenter)
+                    self.tableWidget.setItem(i, j, newItem)
+        else:
+            pass
+
+    def buttonState(self):
+        radioButton = self.sender()
+
+        if radioButton.isChecked() == True:
+            print('<' + radioButton.text() + '> 被选中')
+        else:
+            print('<' + radioButton.text() + '> 被取消选中状态')
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
